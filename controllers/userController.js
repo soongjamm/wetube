83663c20a79b934da6963c3236f2d9e380ee57c2
kimstz0303@gmail.com <soongjamm@userui-MacBookPro.local>
2019-10-18 16:30:30 +0900

    #18. Search Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-export const join = (req, res) => res.render("JOIN");
-export const login = (req, res) => res.render("LOGIN");
-export const logout = (req, res) => res.render("LOGOUT");
-export const users = (req, res) => res.render("USERS");
-export const userDetail = (req, res) => res.render("USER_DETAIL");
-export const editProfile = (req, res) => res.render("EDIT_PROFILE");
-export const changePassword = (req, res) => res.render("CHANGE_PASSWORD");
+export const join = (req, res) => res.render("JOIN", { pageTitle: "Join"});
+export const login = (req, res) => res.render("LOGIN", { pageTitle: "Login"});
+export const logout = (req, res) => res.render("LOGOUT", { pageTitle: "Logout"});
+export const users = (req, res) => res.render("USERS", { pageTitle: "Users"});
+export const userDetail = (req, res) => res.render("USER_DETAIL", { pageTitle: "User Detail"});
+export const editProfile = (req, res) => res.render("EDIT_PROFILE", { pageTitle: "Edit Profile"});
+export const changePassword = (req, res) => res.render("CHANGE_PASSWORD", { pageTitle: "Change Password"});
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,12 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const videos = (req, res) => res.render("videos");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
-export const editVideo = (req, res) => res.render("editVideo");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const upload = (req, res) => res.render("upload");
+export const home = (req, res) => res.render("home", {pageTitle: "Home"});
+export const search = (req, res) => {
+    const {query: {term: searchingBy}
+    } = req;
+    res.render("search", {pageTitle: "Search", searchingBy});
+}
+
+export const videos = (req, res) => res.render("videos", {pageTitle: "Upload"});
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "Delete Video"});
+export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "Edit Video"});
+export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Video Deatil"});
+export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"});
