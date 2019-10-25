2f416d8df172047ec0eed91df6bdf48a29241722
kimstz0303@gmail.com <soongjamm@userui-MacBookPro.local>
2019-10-25 17:07:02 +0900

    24.Login, Profile Controller

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "../routes";
-import { login, logout, getJoin, postJoin} from "../controllers/userController";
+import { getLogin, postLogin, logout, getJoin, postJoin} from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
@@ -8,8 +8,10 @@ const globalRouter = express.Router();
 globalRouter.get(routes.join, getJoin );
 globalRouter.post(routes.join, postJoin);
 
+globalRouter.get(routes.login, getLogin );
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.login, login );
 globalRouter.get(routes.logout, logout );
 globalRouter.get(routes.search, search );
 
