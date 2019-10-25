60598968c324e6fc25fc2fc799da8870d81036ab
kimstz0303@gmail.com <soongjamm@userui-MacBookPro.local>
2019-10-25 18:18:45 +0900

    25.more controllers

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,17 +1,28 @@
-import {videos} from "../db"
+import { videos } from "../db"
+import routes from "../routes"
 
 export const home = (req, res) => {
     res.render("home", {pageTitle: "Home", videos});
 }
 
 export const search = (req, res) => {
-    const {query: {term: searchingBy}
+    const {
+        query: {term: searchingBy}
     } = req;
     res.render("search", {pageTitle: "Search", searchingBy, videos});
 }
 
 //export const videos = (req, res) => res.render("videos", {pageTitle: "Videos"});
-export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"});
+export const getUpload = (req, res) => {
+    res.render("upload", {pageTitle: "Upload"});
+}
+export const postUpload = (req, res) => {
+    const {
+        body: {file, title, description}
+    }=req;
+    // To Do: Upload and save video
+    res.redirect(routes.videoDetail(312313));
+}
 export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "Delete Video"});
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "Edit Video"});
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Video Deatil"});
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,14 +1,15 @@
 import express from "express";
 import routes from "../routes";
-import { videos, deleteVideo, editVideo, videoDetail, upload } from "../controllers/videoController";
+import { getUpload, postUpload, deleteVideo, editVideo, videoDetail, upload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 //videoRouter.get(routes.videos, videos);
-videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.videoDetail(), videoDetail);
 
 
 export default videoRouter;
