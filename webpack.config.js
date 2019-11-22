0340b05521960622d51a17f0b518973d9b013ca5
test <soongjamm@userui-MacBookPro.local>
2019-11-22 13:46:01 +0900

    -#4.3 ES6 with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,29 +7,37 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill",ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
-      {
-        test: /\.(scss)$/,
-        use: ExtractCSS.extract([
-          {
-            loader: "css-loader"
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              plugin() {
-                return [autoprefixer({ browsers: "cover 99.5%" })];
-              }
-            }
-          },
-          {
-            loader: "sass-loader"
-          }
-        ])
-      }
+        {
+            test: /\.(js)$/,
+            use:[
+                {
+                    loader : 'babel-loader'
+                }
+            ]
+        },
+        {
+            test: /\.(scss)$/,
+            use: ExtractCSS.extract([
+            {
+                loader: "css-loader"
+            },
+            {
+                loader: "postcss-loader",
+                options: {
+                    plugins() {
+                        return [autoprefixer({ browsers: "cover 99.5%" })];
+                    }
+                }
+            },
+            {
+                loader: "sass-loader"
+            }   
+            ])
+        }
     ]
   },
   output: {
